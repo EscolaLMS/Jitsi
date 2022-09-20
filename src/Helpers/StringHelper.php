d7220af8fb531716c7b356c4de2aecb08b72e754
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2022-09-20 14:01:19 +0200

    Add jitsi slug map

---
+++ src/Helpers/StringHelper.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace EscolaLms\Jitsi\Helpers;
+
+use EscolaLms\Jitsi\Enum\JitsiEnum;
+
+class StringHelper
+{
+    public function convertToJitsiSlug(string $str, array $options = []): string
+    {
+        // Make sure string is in UTF-8 and strip invalid UTF-8 characters
+        $str = mb_convert_encoding($str, 'UTF-8', mb_list_encodings());
+
+        $defaults = [
+            'delimiter' => '',
+            'limit' => null,
+            'lowercase' => true,
+            'replacements' => [],
+            'transliterate' => true,
+        ];
+
+        // Merge options
+        $options = array_merge($defaults, $options);
+
+        $charMap = JitsiEnum::CHAR_MAP;
+
+        // Make custom replacements
+        $str = preg_replace(array_keys($options['replacements']), $options['replacements'], $str);
+
+        // Transliterate characters to ASCII
+        if ($options['transliterate']) {
+            $str = str_replace(array_keys($charMap), $charMap, $str);
+        }
+
+        // Replace non-alphanumeric characters with our delimiter
+        $str = preg_replace('/[^\p{L}\p{Nd}]+/u', $options['delimiter'], $str);
+
+        // Remove duplicate delimiters
+        $str = preg_replace('/(' . preg_quote($options['delimiter'], '/') . '){2,}/', '$1', $str);
+
+        // Truncate slug to max. characters
+        $str = mb_substr($str, 0, ($options['limit'] ? $options['limit'] : mb_strlen($str, 'UTF-8')), 'UTF-8');
+
+        // Remove delimiter from ends
+        $str = trim($str, $options['delimiter']);
+        return $options['lowercase'] ? mb_strtolower($str, 'UTF-8') : $str;
+    }
+
+}
