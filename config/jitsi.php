b7f54b3ba93cda31737d5dcc9fbe1cc7921d4a9f
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2022-11-04 14:49:02 +0100

    Remove secret key from config

--- config/jitsi.php
+++ config/jitsi.php
@@ -6,7 +6,7 @@ return [
 
     'jitsi_host' => env('JITSI_HOST', 'meet-stage.escolalms.com'),
     'app_id' => env('JITSI_APP_ID', 'meet-id'),
-    'secret' => env('JITSI_APP_SECRET', 'ZKGfn5kYsv47avM4'),
+    'secret' => env('JITSI_APP_SECRET', 'Test'),
 
     'package_status' => PackageStatusEnum::ENABLED,
 
